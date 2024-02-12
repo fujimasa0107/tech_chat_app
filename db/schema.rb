4c3d543c23e8b1d06baa91fafe2aece2a0a4c958
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-02-12 11:41:23 +0900

    モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_02_022619) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_12_021229) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
